5504f085a702f138fc29e1d9d4a297bb8940d338
Mastay <nasywanfadilah@gmail.com>
2023-06-29 21:26:42 +0700

    add image generator feature and some optimization

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 const { Configuration, OpenAIApi } = require("openai");
-const { TiktokDL } = require("@tobyg74/tiktok-api-dl")
 const script = require("./src/file/script.js")
 const qrcode = require('qrcode-terminal');
 const fs = require('fs');
@@ -75,7 +74,7 @@ client.on('message', async (msg) => {
             console.log(`${msg.from} Use command ${prefix}halo. Status : Success`);
         } else if(msg.body.toLowerCase() === `${prefix}help` || msg.body.toLowerCase() === `${prefix}menu`) {
             const media = await MessageMedia.fromUrl("https://i.imgur.com/nFsn61p.png");
-            const menus = `|--------- [ *INFO* ] ---------|\n*Bot Name\t:* _${config.BOT_NAME}_\n*Bot Version\t:* _${config.BOT_VER}_\n*Bot Prefix\t:* _"${prefix}"_\n*Bot Owner\t:* _${config.BOT_OWNER}_\n---------------------------\n\n|===== [ *COMMAND* ] =====|\n=> _*${prefix}halo*_\n=> _*${prefix}help*_\n=> _*${prefix}sticker*_\n=> _*${prefix}ask|<question>*_\n=> _*${prefix}menfes|<nomor telepon>|<nama pengirim>|<pesan>*_\n=> _*${prefix}tiktok <type> <link url tiktok>*_\n=> _*${prefix}ytmp3 <url video>*_\n=> _*${prefix}urlshort <url>*_\n\n_Note : masukkan parameter tanpa simbol "< >"_`
+            const menus = `|--------- [ *INFO* ] ---------|\n*Bot Name\t:* _${config.BOT_NAME}_\n*Bot Version\t:* _${config.BOT_VER}_\n*Bot Prefix\t:* _"${prefix}"_\n*Bot Owner\t:* _${config.BOT_OWNER}_\n---------------------------\n\n|===== [ *COMMAND* ] =====|\n=> _*${prefix}halo*_\n=> _*${prefix}help*_\n=> _*${prefix}sticker*_\n=> _*${prefix}ask <question>*_\n=> _*${prefix}menfes|<nomor telepon>|<nama pengirim>|<pesan>*_\n=> _*${prefix}tiktok <type> <link url tiktok>*_\n=> _*${prefix}ytmp3 <url video>*_\n=> _*${prefix}urlshort <url>*_\n=> _*${prefix}createimage <deskripsi teks>*_\n\n_Note : masukkan parameter tanpa simbol "< >"_`
 
             client.sendMessage(msg.from, media, {
                 caption: menus,
@@ -101,15 +100,15 @@ client.on('message', async (msg) => {
                 msg.reply(`Format salah, pastikan kamu mengirim gambar dengan caption ${prefix}sticker.`)
             }
         } else if (msg.body.startsWith(`${prefix}ask`)) {
-            const params = msg.body.split("|");
+            const params = msg.body.split(" ");
 
-            if(params.length !== 2) {
-                msg.reply(`Format salah, gunakan ${prefix}ask|<pertanyaan>`);
+            if(params.length === 1) {
+                msg.reply(`Format salah, gunakan ${prefix}ask <pertanyaan>`);
                 console.log(`${msg.from} Use command ${prefix}ask. Status : Invalid Parameter`);
                 return;
             }
 
-            const question = params[1].trim();
+            const question = params.slice(1).join(' ');
             const response = await openai.createCompletion({
                 model: "text-davinci-003",
                 prompt: `${question}\n\nBot:`,
@@ -123,6 +122,33 @@ client.on('message', async (msg) => {
 
             msg.reply(response.data.choices[0].text)
             console.log(`${msg.from} Use command ${prefix}ask. Status : Success`);
+
+        } else if(msg.body.startsWith(`${prefix}createimage`)) {
+            const params = msg.body.split(" ");
+
+            if(params.length === 1) {
+                console.log(`${msg.from} Use command ${prefix}createimage. Status : Invalid Parameter`);
+                return msg.reply(`Format salah, gunakan ${prefix}createimage <deskripsi gambar>`);
+            }
+
+            const descImage = params.slice(1).join(' ');
+            const editedDesc = descImage.replace(/ /g, "%20");
+            try {
+                msg.reply("Mohon tunggu....\nOrang sabar disayang zeta😋");
+                const responseImage = `https://daniapi.my.id/api/artificial-intelligence/stable-diffusion?text=${editedDesc}&keys=mastaycuy`;
+                const media = await MessageMedia.fromUrl(responseImage, {
+                    unsafeMime: true,
+                });
+
+                client.sendMessage(msg.from, media, {
+                    caption: `Hasil gambar dengan deskripsi _*${descImage}*_`,
+                });
+                console.log(`${msg.from} Use command ${prefix}createimage. Status : Success`);
+            } catch (err) {
+                msg.reply("Gagal memproses gambar, coba lagi nanti.\n _Jika masalah terus berulang silahkan hubungi Developer_");
+                console.log(`${msg.from} Use command ${prefix}createimage. Status : Error`);
+                console.error(err);
+            }
 
         } else if (msg.body.startsWith(`${prefix}menfes`)) {
             const params = msg.body.split("|");
@@ -164,7 +190,7 @@ client.on('message', async (msg) => {
             const params = msg.body.split(" ");
             if (params.length !== 3) {
                 console.log(`${msg.from} Use command ${prefix}tiktok. Status : Invalid Parameter`);
-                msg.reply(`Gunakan *${prefix}tiktok <type> <url>*.\n_type = video | music_`);
+                msg.reply(`Gunakan *${prefix}tiktok <type> <url>*.\n_type = video | audio_`);
                 return;
             }
             
@@ -174,13 +200,10 @@ client.on('message', async (msg) => {
             
             switch(type) {
                 case 'video':
-                case 'music':
+                case 'audio':
                     try {
                         msg.reply("Mohon tunggu....\nOrang sabar disayang zeta😋");
-                        const defaultUrl = await TiktokDL(url);
-                        // console.log(defaultUrl);
-                        const downloadUrl = defaultUrl.result[type][0];
-                        // console.log(downloadUrl);
+                        const downloadUrl = await script.tiktokDownloader(url, type);
                         const media = await MessageMedia.fromUrl(downloadUrl, {
                             unsafeMime: true,
                         });
@@ -205,8 +228,7 @@ client.on('message', async (msg) => {
 
             if (params.length !== 2) {
                 console.log(`${msg.from} Use command ${prefix}ytmp3. Status: Invalid Parameter`);
-                msg.reply(`Format salah, gunakan ${prefix}ytmp3 <url youtube>`);
-                return;
+                return msg.reply(`Format salah, gunakan ${prefix}ytmp3 <url youtube>`);
             }
 
             msg.reply("Mohon tunggu....\nOrang sabar disayang zeta😋")
@@ -265,11 +287,20 @@ client.on('message', async (msg) => {
                     console.log(`${msg.from} Use command ${prefix}urlshort. Status: Invalid URL`);
                 }
             } catch (error) {
-              msg.reply("Gagal mengakses API");
-              console.log(`${msg.from} Use command ${prefix}urlshort. Status: Failed`, error);
+                msg.reply("Gagal mengakses API");
+                console.log(`${msg.from} Use command ${prefix}urlshort. Status: Failed`, error);
             }
-          }
-          
+        } else {
+            msg.reply(`Command _*${msg.body}*_ tidak tersedia, silahkan gunakan _*${prefix}help*_ atau _*${prefix}menu*_ untuk melihat semua command yang tersedia!`);
+            // axios.get(`https://daniapi.my.id/api/artificial-intelligence/simsimi?text=${msg.body}&lang=id&keys=mastaycuy`) 
+            // .then(response => {
+            //     client.sendMessage(msg.from, response.data.data.answer);
+            // })
+            // .catch(error => {
+            //     console.log(error);
+            //     msg.reply('Maaf, ada kesalahan dalam memproses permintaan Anda.');
+            // });
+        }
     } catch (err) {
         console.error(err);
         client.sendMessage(msg.from, "Ada kesalahan dalam script bot. Silahkan hubungi developer!")
--- src/file/script.js
+++ src/file/script.js
@@ -20,6 +20,31 @@ function urlShortener(url) {
         });
 }
 
+function tiktokDownloader(url, type) {
+    const linkUrl = `https://daniapi.my.id/api/downloader/tiktok?url=${url}&keys=mastaycuy`;
+
+    if(type === 'video') {
+        type = 'video2';
+    }
+
+    return axios.get(linkUrl)
+        .then(response => {
+            return response.data.data[type];
+        })
+        .catch(error => {
+            if (error.response) {
+                console.log("Failed to connect to the API");
+                console.log("Enter a valid URL");
+            } else if (error.request) {
+                console.log("Request error");
+            } else {
+                console.log("Error", error.message);
+            }
+            return null;
+        });
+}
+
 module.exports = {
-    urlShortener: urlShortener
+    urlShortener: urlShortener,
+    tiktokDownloader: tiktokDownloader,
 };
