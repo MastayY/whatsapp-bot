840b5462e388f2710e1a706d6d40ea30e4c8326f
Mastay <nasywanfadilah@gmail.com>
2023-07-06 18:15:09 +0700

    fix

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = { 
    BOT_OWNER: 'Mastay',
    BOT_NAME: 'Mztay Bot',
-   BOT_VER: 'V 1.0.0',
+   BOT_VER: 'V 1.0.1',
    owner_number: '6285643094917',
    ig: '@mastayy.js',
    github: '@MastayY',
